75a4f5b3ebd1444c05a0024cdeb33dd5d607d6bd
Vinodkumar Madhavan <smvinodkumar910@gmail.com>
2025-08-22 02:52:36 +0000

    changes

--- frontend/pages/2_app_rankings.py
+++ frontend/pages/2_app_rankings.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 from tools.read_data import get_review_summary
 
 st.sidebar.header("App Reviews")
@@ -9,12 +10,40 @@ df = get_review_summary()
 
 categories_list = df['app_genre'].unique().tolist()
 
-category = st.selectbox(label='Select App Category', options=categories_list)
+category = st.sidebar.selectbox(label='Select App Category', options=categories_list)
 
 st.subheader(f"Ranking for {category}")
 
-# Placeholder for visualization
-st.bar_chart(df[df['app_genre'] == category]['app_rank'].value_counts())
+filtered_df = df[(df.app_genre==category) & (df.app_rank<=10)][['app_name','app_rank','total_reviews','sentiment_proportion_positive','sentiment_proportion_neutral','sentiment_proportion_negative']].sort_values(by='app_rank',ascending=True)
+filtered_df.rename(columns={'sentiment_proportion_positive':'Positive','sentiment_proportion_neutral':'Neutral','sentiment_proportion_negative':'Negative'},inplace=True)
+
+
+# Create the plot with plotly express
+fig = px.bar(
+    filtered_df, 
+    x=['Positive', 'Neutral', 'Negative'], 
+    y='app_name',
+    text=filtered_df['total_reviews'],
+    orientation='h',
+    title=f"Sentiment Analysis for {category} Apps",
+    labels={'value': '% of sentiment of reviews', 'variable': 'Sentiment'},
+    height=600, # You can adjust this value to fit your labels
+    category_orders={'variable': ['Positive', 'Neutral', 'Negative']}
+)
+
+# Update layout for better readability
+fig.update_layout(
+    yaxis_title='App Name',
+    xaxis_title='% of sentiment of reviews',
+    legend_title='Sentiment',
+    yaxis={'categoryorder':'array', 'categoryarray': filtered_df['app_name'].tolist()[::-1]},
+    uniformtext_minsize=8, 
+    uniformtext_mode='hide'
+)
+
+fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+
+st.plotly_chart(fig, use_container_width=True)
 
 st.subheader("Raw Data")
-st.dataframe(df[df['app_genre'] == category])
+st.dataframe(filtered_df)
